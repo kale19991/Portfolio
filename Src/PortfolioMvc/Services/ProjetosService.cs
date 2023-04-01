e65879c3b7c133b7ff2624b36fefa99a0f29220f
Leonardo Oliveira Notebook <eng_leonardokale@outlook.com>
2023-03-31 23:49:33 -0300

    ajustes projeto

--- Src/PortfolioMvc/Services/ProjetosService.cs
+++ Src/PortfolioMvc/Services/ProjetosService.cs
@@ -9,46 +9,11 @@ public class ProjetosService : IProjetosService
         return new List<Projeto>() {
                 new Projeto
                 {
-                    Titulo = "Macoratti.net",
+                    Titulo = "KalleCOD",
                     Descricao = "Blog de artigos criado em ASP.NET Core desde 2002 com mais de 5000 artigos",
-                    Link = "https://macoratti.net.br",
-                    ImagemURL = "/imagens/macorattinet1.jpg"
-                },
-                    new Projeto
-                {
-                    Titulo = "Curso ASP .NET Core MVC - Vendas",
-                    Descricao = "Curso para criação de um site de vendas de lanches do zero",
-                    Link = "https://www.udemy.com/course/curso-de-asp-net-core-mvc-criando-um-site-do-zero/",
-                    ImagemURL = "/imagens/cursomvc1.jpg"
-                },
-                    new Projeto
-                {
-                    Titulo = "Curso Web API ASP .NET Core Essencial ",
-                    Descricao = "Curso para criar APIs restfull com ASP .NET Core",
-                    Link = "https://www.udemy.com/course/curso-web-api-asp-net-core-essencial/",
-                    ImagemURL = "/imagens/cursowebapi.jpg"
-                },
-                    new Projeto
-                {
-                    Titulo = "Curso Clean Architecture - ASP .NET Core com C#",
-                    Descricao = "Criando projetos .NET Core segundo a Clean Architecture ",
-                    Link = "https://www.udemy.com/course/clean-architecture-essencial-asp-net-core-com-c/",
-                    ImagemURL = "/imagens/cursocleanarch1.jpg"
-                },
-                    new Projeto
-                {
-                    Titulo = "Curso Blazor Essencial (Atualização .NET 6)",
-                    Descricao = "Os principais recursos do Blazor para criar aplicações web usando a linguagem C#",
-                    Link = "https://www.udemy.com/course/curso-blazor-essencial/",
-                    ImagemURL = "/imagens/cursoblazor1.jpg"
-                },
-                    new Projeto
-                {
-                    Titulo = "Docker essencial para a plataforma .NET",
-                    Descricao = "Usando o Docker em aplicações ASP .NET Core MVC",
-                    Link = "https://www.udemy.com/course/docker-essencial-para-a-plataforma-net/",
-                    ImagemURL = "/imagens/cursodocker1.jpg"
-                },
+                    Link = "https://dev.azure.com/kalleartes/KalleCOD",
+                    ImagemURL = "https://www.gravatar.com/userimage/213976415/ff686318b8545b09eac6275b9d4ef901?size=120"
+                }
             };
     }
 }
